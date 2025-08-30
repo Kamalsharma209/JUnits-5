52e94b00d511865ee3a6e8fc12c98c70ad72b11d
Your Name <Kamalsharma4769@gmail.com>
2025-08-30 18:32:28 +0530

    first commit

---
+++ src/test/java/org/example/MathUtilsTest.java
@@ -0,0 +1,11 @@
+package 
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class MathUtilsTest {
+    @Test
+    public void test(){
+        fail("Not yet Implemented");
+    }
+}
